10bf8f69d8e7e6bc9429615d9e94273a26c6ffb9
saip4 <120297734+saip4@users.noreply.github.com>
2024-03-14 20:34:25 +0530

    Add files via upload

---
+++ genkey.py
@@ -0,0 +1,35 @@
+import random
+
+base = 7
+p = 503855227731518752419232924507       
+
+def priv():
+    priv_key=random.randint(1e100,9e100)
+    return priv_key
+
+def mod(a, b, n) : #a**b%n
+    md = a%n
+    if b==1 :
+        return md
+    r = mod(a, b//2, n)
+    if b%2 :
+        md = (md * r**2)%n
+    else :
+        md = (r**2)%n
+    return md
+
+def GenPrime() :
+    while True :
+        n = random.randint(1E200, 9E200)
+        if not n%2 :
+            continue
+        isPrime = True
+        for i in range(100) :
+            a = random.randint(2, n)
+            x  = mod(a, n-1, n)
+            if x != 1 :
+                isPrime = False
+                break
+        if isPrime :
+            break
+    return n
